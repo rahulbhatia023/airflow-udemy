4ee69346352064adea80fc8caa41189af178fea8
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-06-08 15:56:13 +0530

    Adding great expectations dag

---
+++ dags/dag_great_expectations_demo.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+
+from airflow.models import DAG
+from great_expectations_provider.operators.great_expectations import (
+    GreatExpectationsOperator,
+)
+
+ge_root_dir = "/Users/rahulbhatia/Documents/intellij_workspace/great-expectations-demo/great_expectations"
+default_args = {"start_date": datetime(2020, 1, 1)}
+
+with DAG(
+    dag_id="great_expectations_demo",
+    schedule_interval="@daily",
+    catchup=False,
+    default_args=default_args,
+) as dag:
+    ge_filesystem_validate = GreatExpectationsOperator(
+        task_id="ge_filesystem_validate",
+        data_context_root_dir=ge_root_dir,
+        checkpoint_name="checkpoint_filesystem_demo",
+        return_json_dict=True,
+    )
+
+    ge_filesystem_profiler_validate = GreatExpectationsOperator(
+        task_id="ge_filesystem_profiler_validate",
+        data_context_root_dir=ge_root_dir,
+        checkpoint_name="checkpoint_filesystem_profiler_demo",
+        return_json_dict=True,
+    )
+
+    ge_postgres_validate = GreatExpectationsOperator(
+        task_id="ge_postgres_validate",
+        data_context_root_dir=ge_root_dir,
+        checkpoint_name="checkpoint_postgres_demo",
+        return_json_dict=True,
+    )
+
+    ge_filesystem_validate >> ge_filesystem_profiler_validate >> ge_postgres_validate
---
+++ dags/dag_parallel_tasks.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {"start_date": datetime(2020, 1, 1)}
+
+with DAG(
+    dag_id="parallel_dag",
+    schedule_interval="@daily",
+    catchup=False,
+    default_args=default_args,
+) as dag:
+    task_1 = BashOperator(
+        task_id="task_1",
+        bash_command="sleep 3",
+    )
+
+    task_2 = BashOperator(
+        task_id="task_2",
+        bash_command="sleep 3",
+    )
+
+    task_3 = BashOperator(
+        task_id="task_3",
+        bash_command="sleep 3",
+    )
+
+    task_4 = BashOperator(
+        task_id="task_4",
+        bash_command="sleep 3",
+    )
+
+    task_1 >> [task_2, task_3] >> task_4
