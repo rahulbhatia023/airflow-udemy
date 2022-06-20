ee71f9ba2de2fde0436d4617b8e97dd9c4ebc976
rahulbhatia023 <rahul.bhatia023@gmail.com>
2022-06-20 19:51:24 +0530

    Xcom

--- dags/dag_parallel_tasks.py
+++ dags/dag_parallel_tasks.py
@@ -2,6 +2,7 @@ from datetime import datetime
 
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
+from airflow.utils.task_group import TaskGroup
 
 default_args = {"start_date": datetime(2020, 1, 1)}
 
@@ -16,19 +17,20 @@ with DAG(
         bash_command="sleep 3",
     )
 
-    task_2 = BashOperator(
-        task_id="task_2",
-        bash_command="sleep 3",
-    )
+    with TaskGroup(group_id="processing_task_group") as processing_task_group:
+        task_2 = BashOperator(
+            task_id="task_2",
+            bash_command="sleep 3",
+        )
 
-    task_3 = BashOperator(
-        task_id="task_3",
-        bash_command="sleep 3",
-    )
+        task_3 = BashOperator(
+            task_id="task_3",
+            bash_command="sleep 3",
+        )
 
     task_4 = BashOperator(
         task_id="task_4",
         bash_command="sleep 3",
     )
 
-    task_1 >> [task_2, task_3] >> task_4
+    task_1 >> processing_task_group >> task_4
